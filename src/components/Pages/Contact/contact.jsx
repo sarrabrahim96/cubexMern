ae7ebe6a8650e55d434ed3fd69a4179851cd5334
sarrabrahim96 <brahimsarah43@gmail.com>
2023-02-24 15:16:24 +0100

    first commit

---
+++ src/components/Pages/Contact/contact.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Spacing from "../../Spacing";
+import Div from "../../Div";
+import { pageTitle } from "../../../helper";
+import {saveAs} from 'file-saver'
+import axios from "axios";
+
+function Contact() {
+
+  pageTitle("Contact");
+  const [Contact, setContact] = useState([]);
+
+  useEffect(() => {
+    const getContact = async () => {
+      const res = await axios.get("/contact/getcontact"); 
+      setContact(res.data.response);
+    };
+    getContact();
+  }, []);
+
+  return (
+    <>
+      <Spacing lg="110" md="80" />
+      <Div className="container">
+        <Div className="col-lg-12">
+          <table>
+            <tr>
+              <th>Nom et prénom</th>
+              <th>Email</th>
+              <th>Numéro de téléphone</th>
+              <th>Message de description</th>
+              <th>CV </th>
+            </tr>
+            {Contact?.map((item,index)=>(
+                                <tr key={index}>
+                                <td>{item.username}</td>
+                                <td>{item.email}</td>
+                                <td>{item.numtel}</td>
+                                <td>{item.msg}</td>
+                                <td>
+                                  <img src={item.image} style={{width:'100%' , height : "100%"}}></img>
+                                </td>
+                              </tr>
+              ))} 
+
+          </table>
+        </Div>
+      </Div>
+    </>
+  );
+}
+
+export default Contact;
